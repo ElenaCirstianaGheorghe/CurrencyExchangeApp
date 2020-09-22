07226560fe57140c11654d830dca35c9c51cde32
ElenaCirstianaGheorghe <71639610+ElenaCirstianaGheorghe@users.noreply.github.com>
2020-09-22 12:42:38 +0300

    Update ConvertingApp_kivymd.py

--- ConvertingApp_kivymd.py
+++ ConvertingApp_kivymd.py
@@ -15,7 +15,7 @@ class ConvertingApp(MDApp):
 
     icons = []
     for i in range(33):
-        icons.append(f"Flags\{i}.png")  #don't forget to write the path for the directory where the Flags file is stored 
+        icons.append(f"Flags\{i}.png")  #don't forget to write the path of the directory where the Flags file is stored 
 
     def build(self):
         self.theme_cls.primary_palette = "Teal"
